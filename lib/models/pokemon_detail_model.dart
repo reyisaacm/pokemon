d2668a12c99030d98c588c349fa8e64a77b39b8e
Reynhart <reynhart_i_malingkas@telkomsel.co.id>
2024-03-13 13:46:53 +0700

    fix test

--- lib/models/pokemon_detail_model.dart
+++ lib/models/pokemon_detail_model.dart
@@ -69,4 +69,66 @@ class PokemonDetailModel {
   }
 
   String toJson() => json.encode(toMap());
+
+  @override
+  bool operator ==(covariant PokemonDetailModel other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id &&
+        other.name == name &&
+        other.imageUrl == imageUrl &&
+        other.hp == hp &&
+        other.attack == attack &&
+        other.defense == defense &&
+        other.specialAttack == specialAttack &&
+        other.specialDefense == specialDefense &&
+        other.speed == speed &&
+        other.evolution == evolution &&
+        other.weight == weight &&
+        other.previousBerryType == previousBerryType;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        name.hashCode ^
+        imageUrl.hashCode ^
+        hp.hashCode ^
+        attack.hashCode ^
+        defense.hashCode ^
+        specialAttack.hashCode ^
+        specialDefense.hashCode ^
+        speed.hashCode ^
+        evolution.hashCode ^
+        weight.hashCode ^
+        previousBerryType.hashCode;
+  }
+
+  factory PokemonDetailModel.fromMap(Map<String, dynamic> map) {
+    return PokemonDetailModel(
+      id: map['id'] as int,
+      name: map['name'] as String,
+      imageUrl: map['imageUrl'] as String,
+      hp: map['hp'] as int,
+      attack: map['attack'] as int,
+      defense: map['defense'] as int,
+      specialAttack: map['specialAttack'] as int,
+      specialDefense: map['specialDefense'] as int,
+      speed: map['speed'] as int,
+      evolution: map['evolution'] != null
+          ? PokemonSpeciesEvolutionChainModel.fromMap(
+              map['evolution'] as Map<String, dynamic>)
+          : null,
+      weight: map['weight'] as int,
+      previousBerryType: map['previousBerryType'] as String,
+    );
+  }
+
+  factory PokemonDetailModel.fromJson(String source) =>
+      PokemonDetailModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'PokemonDetailModel(id: $id, name: $name, imageUrl: $imageUrl, hp: $hp, attack: $attack, defense: $defense, specialAttack: $specialAttack, specialDefense: $specialDefense, speed: $speed, evolution: $evolution, weight: $weight, previousBerryType: $previousBerryType)';
+  }
 }
